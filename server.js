91358d3f2e52aef05283f428dbbecbf6c0e3daf4
jpardike2 <jpardike@gmail.com>
2020-10-09 13:33:47 -0700

    Server boilerplate

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+// View Engine
+app.set('view engine', 'ejs');
+
+
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
