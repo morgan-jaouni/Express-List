edc5c91fa14948551145d45b96e4cfd8b5067ef0
jpardike2 <jpardike@gmail.com>
2020-10-09 15:25:18 -0700

    Setup post for new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,10 +6,16 @@ const db = require('../models');
 
 //View users route
 router.get('/', (req,res) => {
+  db.User.find({}, (err, allUsers) => {
+    if (err) return console.log(err);
 
-    res.render('users/index')
+    const context = { users: allUsers };
 
+    res.render('users/index', context);
+  });
 });
+
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const methodOverride = require('method-override');
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
+// Database
+const db = require('./models');
+
 // View Engine
 app.set('view engine', 'ejs');
 
@@ -13,11 +16,27 @@ app.set('view engine', 'ejs');
 const ctrl = require('./controllers');
 
 
+// Middleware
+
+// body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 // Routes
 
 // Home Route
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// POST New user
+app.post('/users', (req, res) => {
+  db.User.create(req.body, (err, newUser) => {
+    if (err) return console.log(err);
+
+    res.redirect('/users');
+  })
 });
 
 //Products Index
