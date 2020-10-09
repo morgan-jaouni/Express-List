bf3ef2a6a1d73ae17222d9996b499c794de4fac3
jpardike2 <jpardike@gmail.com>
2020-10-09 15:56:56 -0700

    Adds new product page

---
+++ models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  condition: String,
+  image: String,
+});
+
+const User = mongoose.model('Product', productSchema);
+
+
+module.exports = Product;
