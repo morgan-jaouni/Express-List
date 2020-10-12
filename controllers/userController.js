4b63e62b8ce480214daff91ea141ce5bfe645c6d
morganjaouni <morgan.jaouni@ttu.edu>
2020-10-12 13:17:12 -0500

    Adds update route for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,8 +62,31 @@ router.delete('/:userId', (req,res) => {
   });
 });
 
+// GET User  Edit
 
-//User Update
+router.get('/:userId/edit', (req, res) =>{
+  db.User.findById(req.params.userId, (err, foundUser) => {
+      if (err) return console.log(err);
+
+      const context = { user: foundUser };
+
+      res.render('users/edit', context);
+  });
+});
+
+// PUT edit
+router.put('/:userId', (req, res) => {
+  db.User.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true },
+      (err, updatedUser) => {
+          if (err) return console.log(err);
+          res.redirect(`/users/${updatedUser.id}`);
+      }
+  );
+});
+
 
 
 module.exports = router;
